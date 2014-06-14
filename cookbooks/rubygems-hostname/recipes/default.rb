6769c2b75ccb7db3e499ce24a048b9810268cf60
David Radcliffe <radcliffe.david@gmail.com>
2014-06-14 10:41:56 -0400

    cleanup

--- cookbooks/rubygems-hostname/recipes/default.rb
+++ cookbooks/rubygems-hostname/recipes/default.rb
@@ -1,3 +1,8 @@
+#
+# Cookbook Name:: rubygems-hostname
+# Recipe:: default
+#
+
 node.default['set_fqdn'] = node['name']
 
 include_recipe 'hostname'
--- cookbooks/rubygems/recipes/default.rb
+++ cookbooks/rubygems/recipes/default.rb
@@ -1,5 +1,10 @@
-include_recipe "rubygems-hostname"
-include_recipe "rubygems-motd"
-include_recipe "rubygems-ntp"
-include_recipe "rubygems-people"
-include_recipe "rubygems-utility"
+#
+# Cookbook Name:: rubygems
+# Recipe:: default
+#
+
+include_recipe 'rubygems-hostname'
+include_recipe 'rubygems-motd'
+include_recipe 'rubygems-ntp'
+include_recipe 'rubygems-people'
+include_recipe 'rubygems-utility'
