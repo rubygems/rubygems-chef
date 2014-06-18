c05d697612de4a55ec57a77c9cea65b13483d236
David Radcliffe <radcliffe.david@gmail.com>
2014-06-18 16:36:19 -0400

    cleanup unicorn/delayed_job

--- cookbooks/rubygems-app/recipes/delayed_job.rb
+++ cookbooks/rubygems-app/recipes/delayed_job.rb
@@ -3,7 +3,21 @@
 # Recipe:: delayed_job
 #
 
+redis_ip = search(:node, "name:redis01.#{node.chef_environment}.rubygems.org")[0]['ipaddress']
+
 runit_service 'delayed_job' do
-  env node['environment_variables']
+  owner 'deploy'
+  group 'deploy'
+  default_logger true
+  env(
+    'RAILS_ENV' => node.chef_environment,
+    'REDISTOGO_URL' => "redis://#{redis_ip}:6379/0"
+  )
+  options(
+    owner: 'deploy',
+    group: 'deploy',
+    bundle_command: '/usr/local/bin/bundle',
+    rails_env: node.chef_environment,
+  )
   action ::File.exist?('/applications/rubygems/current') ? :enable : :disable
 end
--- cookbooks/rubygems-app/recipes/unicorn.rb
+++ cookbooks/rubygems-app/recipes/unicorn.rb
@@ -15,12 +15,14 @@ redis_ip = search(:node, "name:redis01.#{node.chef_environment}.rubygems.org")[0
 runit_service 'unicorn' do
   owner 'deploy'
   group 'deploy'
+  default_logger true
+  env(
+    'RAILS_ENV' => node.chef_environment,
+    'REDISTOGO_URL' => "redis://#{redis_ip}:6379/0"
+  )
   options(
-    name: 'rubygems',
     owner: 'deploy',
     group: 'deploy',
-    redis_url: "redis://#{redis_ip}:6379/0",
-    bundler: true,
     bundle_command: '/usr/local/bin/bundle',
     rails_env: node.chef_environment,
     smells_like_rack: ::File.exist?('/applications/rubygems/current/config.ru')
