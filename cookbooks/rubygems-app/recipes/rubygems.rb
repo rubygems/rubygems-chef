2f6452b768bc62c43c15bd597dedfe9bde1a100d
David Radcliffe <radcliffe.david@gmail.com>
2014-06-18 13:42:02 -0400

    install rubygems 2.2.2

---
+++ cookbooks/rubygems-app/recipes/rubygems.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: rubygems-app
+# Recipe:: rubygems
+#
+
+node.default['rubygems']['rubygems_version'] = '2.2.2'
+
+execute "gem update --system #{node['rubygems']['rubygems_version']}" do
+  action :run
+  environment 'REALLY_GEM_UPDATE_SYSTEM' => 'true'
+  not_if "gem -v | grep -q '#{node['rubygems']['rubygems_version']}'"
+end
---
+++ cookbooks/rubygems-app/test/integration/default/serverspec/rubygems_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe 'rubygems-app::rubygems' do
+
+  describe command('gem -v') do
+    it { should return_stdout(/2\.2\.2/) }
+  end
+
+end
