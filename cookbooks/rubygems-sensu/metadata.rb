1014216c3dfd7066dff4aed21b42fdcf5b056eb5
Sam Kottler <shk@linux.com>
2014-06-19 08:18:42 +0000

    Run check_ntp_time

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.39'
+version '0.0.40'
 
 depends 'chef-vault'
 depends 'sensu'
--- cookbooks/rubygems-sensu/recipes/base.rb
+++ cookbooks/rubygems-sensu/recipes/base.rb
@@ -1,40 +1,48 @@
-gem_package "sensu-plugin" do
-  gem_binary "/opt/sensu/embedded/bin/gem"
+gem_package 'sensu-plugin' do
+  gem_binary '/opt/sensu/embedded/bin/gem'
 end
 
-package "nagios-plugins"
+package 'nagios-plugins'
 
 %w{ check-procs.rb }.each do |plugin|
   cookbook_file "/etc/sensu/plugins/#{plugin}" do
     source plugin
     path "/etc/sensu/plugins/#{plugin}"
-    owner "sensu"
-    group "sensu"
-    mode "0755"
+    owner 'sensu'
+    group 'sensu'
+    mode '0755'
     action :create
   end
 end
 
-sensu_check "check_procs" do
-  command "/opt/sensu/embedded/bin/ruby /etc/sensu/plugins/check-procs.rb"
-  handlers ["default", "slack"]
-  subscribers ["all"]
+sensu_check 'check_procs' do
+  command '/opt/sensu/embedded/bin/ruby /etc/sensu/plugins/check-procs.rb'
+  handlers ['default', 'slack']
+  subscribers ['all']
   interval 30
   additional(:notification => 'There is a high number of procs running', :occurences => 3)
 end
 
-sensu_check "check_ssh" do
-  command "/usr/lib/nagios/plugins/check_ssh localhost"
-  handlers ["default", "slack"]
-  subscribers ["all"]
+sensu_check 'check_ssh' do
+  command '/usr/lib/nagios/plugins/check_ssh localhost'
+  handlers ['default', 'slack']
+  subscribers ['all']
   interval 30
   additional(:notification => 'sshd is not running', :occurences => 3)
 end
 
-sensu_check "check_apt" do
-  command "/usr/lib/nagios/plugins/check_apt"
+sensu_check 'check_apt' do
+  command '/usr/lib/nagios/plugins/check_apt'
   handlers ['default', 'slack']
   subscribers ['all']
   interval 60
   additional(:notification => 'There are pending package upgrades', :occurences => 3)
+end
+
+sensu_check 'check_ntp_time' do
+  command '/usr/lib/nagios/plugins/check_ntp_time -H localhost'
+  handlers ['default', 'slack']
+  subscribers ['all']
+  interval 30
+  additional(:notification => 'NTP is out of sync', :occurences => 3)
 end
