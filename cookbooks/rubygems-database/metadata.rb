27c36bfa60d8189c152142c1a794dc12a20fe217
Sam Kottler <shk@linux.com>
2014-06-19 10:54:57 +0000

    Get the database cookbook working (still needs tweaking)

--- cookbooks/rubygems-database/metadata.rb
+++ cookbooks/rubygems-database/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-database'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.7'
+version '0.0.14'
 
 depends 'database'
 depends 'postgresql'
--- cookbooks/rubygems-database/recipes/default.rb
+++ cookbooks/rubygems-database/recipes/default.rb
@@ -1,5 +1,7 @@
 include_recipe 'rubygems'
 
+node.default['postgresql']['password']['postgres'] = ''
+
 include_recipe 'postgresql::server'
 include_recipe 'postgresql::ruby'
 
@@ -8,17 +10,18 @@ stage = node.chef_environment
 connection_info =  {
   :host => '127.0.0.1',
   :port => 5432,
-  :username => "postgres",
-  :password => ""
+  :username => 'postgres'
 }
-
-postgresql_database "rubygems_#{stage}" do
-  connection(connection_info)
-  action :create
-end
 
 postgresql_database_user "rubygems_#{stage}" do
   connection(connection_info)
-  password ""
+  password ''
+  action :create
+end
+
+connection_info['username'] = "rubygems_#{stage}"
+
+postgresql_database "rubygems_#{stage}" do
+  connection(connection_info)
   action :create
 end
