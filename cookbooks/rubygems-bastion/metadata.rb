69d4fae23646627582835f250c25e4a7f38c5496
David Radcliffe <radcliffe.david@gmail.com>
2014-06-20 09:14:02 -0400

    add missing dependancies

--- cookbooks/rubygems-bastion/metadata.rb
+++ cookbooks/rubygems-bastion/metadata.rb
@@ -1,8 +1,9 @@
 name 'rubygems-bastion'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.4'
+version '0.0.5'
 
+depends 'chef-vault'
 depends 'duo-security'
 depends 'rubygems'
 
--- cookbooks/rubygems-notify/metadata.rb
+++ cookbooks/rubygems-notify/metadata.rb
@@ -1,8 +1,9 @@
 name 'rubygems-notify'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.2'
+version '0.0.3'
 
+depends 'chef-vault'
 depends 'slack_handler'
 
 supports 'ubuntu'
