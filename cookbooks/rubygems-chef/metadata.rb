8c66323549bf0956e36d0f011425b5baa4e5cd73
Sam Kottler <shk@linux.com>
2014-06-20 18:36:20 +0000

    Run chef-client every 30 minutes instead of every 15

--- cookbooks/rubygems-chef/metadata.rb
+++ cookbooks/rubygems-chef/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-chef'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.2'
+version '0.0.3'
 
 depends 'line'
 
--- cookbooks/rubygems-chef/recipes/default.rb
+++ cookbooks/rubygems-chef/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 cron 'chef-client' do
-  minute  '*/15'
+  minute  '*/30'
   hour    '*'
   user    'root'
   command '/usr/bin/chef-client'
