bdd6fea5c7e6cabdb8ea90a9e7433dffa0401e92
Sam Kottler <shk@linux.com>
2014-06-20 19:00:42 +0000

    Enable client_fork and include chef-client::config

--- cookbooks/rubygems-chef/recipes/default.rb
+++ cookbooks/rubygems-chef/recipes/default.rb
@@ -7,6 +7,8 @@ cron 'chef-client' do
   action :delete
 end
 
-node.default['chef_client']['config'] = { 'ssl_verify_mode' => ':verify_peer' }
+node.default['chef_client']['config']['ssl_verify_mode'] = ':verify_peer'
+node.default['chef_client']['config']['client_fork'] = true
 
+include_recipe 'chef-client::config'
 include_recipe 'chef-client'
