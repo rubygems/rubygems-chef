d2210a7d464acdd7156fa536e23ebc46477c2b2c
Sam Kottler <shk@linux.com>
2014-06-23 17:27:34 +0000

    Contrary to the rsyslog cookbook's readme, PreserveFQDN is not a bool

--- cookbooks/rubygems-logging/metadata.rb
+++ cookbooks/rubygems-logging/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-logging'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.4'
+version '0.0.5'
 
 depends 'chef-vault'
 depends 'rsyslog'
--- cookbooks/rubygems-logging/recipes/default.rb
+++ cookbooks/rubygems-logging/recipes/default.rb
@@ -4,7 +4,7 @@ papertrail_creds = chef_vault_item('papertrail', 'credentials')
 
 node.default['rsyslog']['server_ip'] = papertrail_creds['server']
 node.default['rsyslog']['port'] = papertrail_creds['port']
-node.default['rsyslog']['preserve_fqdn'] = true
+node.default['rsyslog']['preserve_fqdn'] = 'on'
 node.default['rsyslog']['high_precision_timestamps'] = true
 
 include_recipe 'rsyslog::client'
