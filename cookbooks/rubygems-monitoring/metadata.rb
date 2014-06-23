0581c0562e22993b0d42639c41624933af7e240b
Sam Kottler <shk@linux.com>
2014-06-23 14:23:47 +0000

    CNAME monitoring.rubygems.org to monitoring<xx>.common.rubygems.org

--- cookbooks/rubygems-monitoring/metadata.rb
+++ cookbooks/rubygems-monitoring/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-monitoring'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.3'
+version '0.0.7'
 
 depends 'rubygems'
 depends 'rubygems-sensu'
--- cookbooks/rubygems-monitoring/recipes/default.rb
+++ cookbooks/rubygems-monitoring/recipes/default.rb
@@ -3,5 +3,21 @@
 # Recipe:: default
 #
 
+include_recipe 'chef-vault'
+
+dnsimple_credentials = chef_vault_item('dnsimple', 'credentials')
+
+include_recipe 'dwradcliffe-dnsimple'
+
+dwradcliffe_dnsimple_record "create CNAME point monitoring.rubygems.org to #{node.name}" do
+  name     'monitoring.rubygems.org'
+  content  node['cloud_v2']['public_hostname']
+  type     'CNAME'
+  domain   'rubygems.org'
+  username dnsimple_credentials['username']
+  password dnsimple_credentials['password']
+  action   :create
+end
+
 include_recipe 'rubygems-sensu::server'
 include_recipe 'rubygems'
