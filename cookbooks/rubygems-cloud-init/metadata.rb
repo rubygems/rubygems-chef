09fd9e28f9007df19baa8e4f63bbfa07aba01c25
Sam Kottler <shk@linux.com>
2014-06-29 16:45:25 +0000

    Add rubygems-cloud-init cookbook

---
+++ cookbooks/rubygems-cloud-init/metadata.rb
@@ -0,0 +1,6 @@
+name 'rubygems-cloud-init'
+maintainer 'RubyGems.org ops team'
+
+version '0.0.1'
+
+supports 'ubuntu'
---
+++ cookbooks/rubygems-cloud-init/recipes/default.rb
@@ -0,0 +1,3 @@
+cookbook_file 'cloud.cfg' do
+  path '/etc/cloud/cloud.cfg'
+end
