cef08a8821cacea9144fd69ce36a5a5616be692f
Sam Kottler <shk@linux.com>
2014-06-30 05:22:03 +0000

    Temporarily allow unauthenticated (unsigned) package installation

--- cookbooks/rubygems-apt/metadata.rb
+++ cookbooks/rubygems-apt/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-apt'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.2'
+version '0.0.4'
 
 depends 'apt'
 
--- cookbooks/rubygems-apt/recipes/default.rb
+++ cookbooks/rubygems-apt/recipes/default.rb
@@ -3,6 +3,11 @@ file '/etc/apt/sources.list' do
   action :delete
 end
 
+file '/etc/apt/apt.conf.d/05unauthenticated' do
+  action :create
+  content "APT::Get::AllowUnauthenticated 'true';"
+end
+
 apt_repository 'rubygems_main' do
   uri 'http://repo01.common.rubygems.org'
   distribution node['lsb']['codename']
