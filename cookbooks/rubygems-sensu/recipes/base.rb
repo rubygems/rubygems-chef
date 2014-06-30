94ee1069ec2483e3e9bf07fe657aa4d425f12c66
Sam Kottler <shk@linux.com>
2014-06-30 11:28:13 +0000

    Use the right omnibus package for the collectd check

--- cookbooks/rubygems-sensu/recipes/base.rb
+++ cookbooks/rubygems-sensu/recipes/base.rb
@@ -37,7 +37,7 @@ sensu_check 'check_ntpd_proc' do
 end
 
 sensu_check 'check_collectd_proc' do
-  command "/opt/chef/embedded/bin/ruby /etc/sensu/plugins/check-procs.rb -p 'collectd -C /etc/collectd/collectd.conf'"
+  command "/opt/sensu/embedded/bin/ruby /etc/sensu/plugins/check-procs.rb -p 'collectd -C /etc/collectd/collectd.conf'"
   handlers ['slack']
   subscribers ['all']
   interval 30
