2e37b7dd5c82c4570fe3f7f7d9a251a429d67e82
Sam Kottler <shk@linux.com>
2014-07-03 23:30:18 +0600

    Only create a CNAME if node[cloud_v2] is not nil
    When nodes are running in VPC, ohai will set `cloud_v2` to nil. Since VPC nodes don't have public CNAME's (only a public IP), ultimately we'll need to create A-records.

--- cookbooks/rubygems-hostname/recipes/dns.rb
+++ cookbooks/rubygems-hostname/recipes/dns.rb
@@ -9,12 +9,14 @@ dnsimple_credentials = chef_vault_item('dnsimple', 'credentials')
 
 include_recipe 'dwradcliffe-dnsimple'
 
-dwradcliffe_dnsimple_record "create a CNAME record for #{node.name}" do
-  name     node.name.sub('.rubygems.org', '')
-  content  node['cloud_v2']['public_hostname']
-  type     'CNAME'
-  domain   'rubygems.org'
-  username dnsimple_credentials['username']
-  password dnsimple_credentials['password']
-  action   :create
+if node['cloud_v2']
+  dwradcliffe_dnsimple_record "create a CNAME record for #{node.name}" do
+    name     node.name.sub('.rubygems.org', '')
+    content  node['cloud_v2']['public_hostname']
+    type     'CNAME'
+    domain   'rubygems.org'
+    username dnsimple_credentials['username']
+    password dnsimple_credentials['password']
+    action   :create
+  end
 end
