27467816592f8cbcc330be68fa7266892066cbf3
David Radcliffe <radcliffe.david@gmail.com>
2014-08-07 09:20:33 -0400

    back to sensu 1.0.0 for now

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,9 +1,9 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.73'
+version '0.0.74'
 
 depends 'chef-vault'
-depends 'sensu'
+depends 'sensu', '1.0.0'
 
 supports 'ubuntu'
