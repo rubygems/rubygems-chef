2cb9175bebccb16da131d88501bf4ab7de9f5c9e
Sam Kottler <shk@linux.com>
2014-09-01 16:13:43 -0400

    Add missing depends

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,9 +1,10 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.80'
+version '0.0.81'
 
 depends 'chef-vault'
+depends 'cpan'
 depends 'sensu', '1.0.0'
 
 supports 'ubuntu'
