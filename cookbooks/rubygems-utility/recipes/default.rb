efc5c9e7093ca3e4268f03494c97b9cfc60be70c
Sam Kottler <shk@linux.com>
2014-09-25 11:52:27 -0700

    some more bash upgrade stuff

--- cookbooks/rubygems-utility/recipes/default.rb
+++ cookbooks/rubygems-utility/recipes/default.rb
@@ -9,9 +9,17 @@ packages.each do |pkg|
   package pkg
 end
 
+include_recipe 'apt'
+
 package 'bash' do
   case node['lsb']['codename']
   when 'trusty'
     version '4.3-7ubuntu1.1'
   end
+  notifies :run, 'execute[run-ldconfig]', :immediately
+end
+
+execute 'run-ldconfig' do
+  command '/sbin/ldconfig'
+  action :nothing
 end
