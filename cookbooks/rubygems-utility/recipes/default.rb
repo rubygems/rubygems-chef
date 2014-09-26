a459379ada9d53dd6b03b903bce134da07ba36f3
David Radcliffe <radcliffe.david@gmail.com>
2014-09-26 10:48:53 -0400

    upgrade bash, again

--- cookbooks/rubygems-utility/recipes/default.rb
+++ cookbooks/rubygems-utility/recipes/default.rb
@@ -14,7 +14,7 @@ include_recipe 'apt'
 package 'bash' do
   case node['lsb']['codename']
   when 'trusty'
-    version '4.3-7ubuntu1.1'
+    version '4.3-7ubuntu1.3'
   end
   notifies :run, 'execute[run-ldconfig]', :immediately
 end
