72ca278b67c7fb0e8b856ef6cffe835891481f39
David Radcliffe <radcliffe.david@gmail.com>
2014-12-23 15:53:18 -0500

    lock librato gem versions because 1.5.0 breaks things

--- cookbooks/rubygems-chef/metadata.rb
+++ cookbooks/rubygems-chef/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-chef'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.32'
+version '0.0.36'
 
 depends 'chef-client'
 depends 'chef-vault'
--- cookbooks/rubygems-chef/recipes/librato.rb
+++ cookbooks/rubygems-chef/recipes/librato.rb
@@ -8,7 +8,13 @@ librato_creds = chef_vault_item('librato', 'credentials')
 
 directory '/etc/chef/client.d'
 
-chef_gem 'chef-handler-librato'
+chef_gem 'librato-metrics' do
+  version '1.4.0'
+end
+
+chef_gem 'chef-handler-librato' do
+  version '1.1.6'
+end
 
 template '/etc/chef/client.d/librato.rb' do
   source 'librato.rb'
