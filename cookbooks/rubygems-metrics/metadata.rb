e67e3f16f37b17486d304f55e2de67f4f76648e6
David Radcliffe <radcliffe.david@gmail.com>
2015-01-28 20:21:31 -0500

    add datadog

--- cookbooks/rubygems-metrics/metadata.rb
+++ cookbooks/rubygems-metrics/metadata.rb
@@ -1,9 +1,10 @@
 name 'rubygems-metrics'
 maintainer 'RubyGems.org ops team'
 
-version '0.0.8'
+version '0.1.1'
 
 depends 'chef-vault'
 depends 'collectd'
 depends 'collectd-librato'
 depends 'collectd_plugins'
+depends 'datadog'
--- cookbooks/rubygems-metrics/recipes/default.rb
+++ cookbooks/rubygems-metrics/recipes/default.rb
@@ -5,6 +5,8 @@
 
 include_recipe 'chef-vault'
 
+# Librato
+
 librato_creds = chef_vault_item('librato', 'credentials')
 
 node.default['collectd_librato']['email'] = librato_creds['email']
@@ -22,3 +24,13 @@ include_recipe 'collectd_plugins::load'
 include_recipe 'collectd_plugins::memory'
 include_recipe 'collectd_plugins::swap'
 include_recipe 'collectd_plugins::syslog'
+
+# Datadog
+
+datadog_creds = chef_vault_item('datadog', 'credentials')
+
+node.default['datadog']['api_key'] = datadog_creds['api_key']
+node.default['datadog']['application_key'] = datadog_creds['application_key']
+
+include_recipe 'datadog::dd-agent'
+include_recipe 'datadog::dd-handler'
