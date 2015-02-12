6d3f6c65645326c2b42939ac2b0ca275cf4a9c26
David Radcliffe <radcliffe.david@gmail.com>
2015-02-11 20:04:35 -0500

    update nginx checks

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.1.7'
+version '0.1.8'
 
 depends 'build-essential'
 depends 'chef-vault'
--- cookbooks/rubygems-sensu/recipes/balancer.rb
+++ cookbooks/rubygems-sensu/recipes/balancer.rb
@@ -4,19 +4,19 @@
 #
 
 sensu_check 'check_nginx_http' do
-  command '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 -t 5 -w 1 -c 2'
+  command '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 -t 5 -w 3 -c 5'
   handlers ['slack', 'pagerduty']
   subscribers ['balancer']
   interval 30
-  additional(notification: "[#{node.chef_environment}] nginx is not returning 200 OK on port 80", occurrences: 3)
+  additional(occurrences: 3)
 end
 
 sensu_check 'check_nginx_https' do
-  command '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 --ssl -t 5 -w 1 -c 2'
+  command '/usr/lib/nagios/plugins/check_http -I 127.0.0.1 --ssl -t 5 -w 3 -c 5'
   handlers ['slack', 'pagerduty']
   subscribers ['balancer']
   interval 30
-  additional(notification: "[#{node.chef_environment}] nginx is not returning 200 OK on port 443", occurrences: 3)
+  additional(occurrences: 3)
 end
 
 sensu_check 'check_nginx_cert_expiration' do
