aeb0675ce27a6a32e186c70fd55de5fab35a787c
David Radcliffe <radcliffe.david@gmail.com>
2015-02-17 22:52:56 -0500

    update uchiwa cookbook

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,12 +1,12 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.1.9'
+version '0.1.10'
 
 depends 'build-essential'
 depends 'chef-vault'
 depends 'cpan'
 depends 'sensu', '2.1.0'
-depends 'uchiwa'
+depends 'uchiwa', '1.0.0'
 
 supports 'ubuntu'
