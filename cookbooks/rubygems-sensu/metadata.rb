5a20f8ad643be4c24faa6e092be1bbbfe93affce
David Radcliffe <radcliffe.david@gmail.com>
2015-03-03 21:48:28 -0500

    upgrade uchiwa package

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,7 +1,7 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.1.13'
+version '0.1.17'
 
 depends 'build-essential'
 depends 'chef-vault'
--- cookbooks/rubygems-sensu/recipes/uchiwa.rb
+++ cookbooks/rubygems-sensu/recipes/uchiwa.rb
@@ -3,11 +3,11 @@
 # Recipe:: uchiwa
 #
 
-node.default['uchiwa']['version'] = '0.3.4-1'
+node.default['uchiwa']['version'] = '0.6.0-1'
 node.default['uchiwa']['settings']['user'] = ''
 node.default['uchiwa']['settings']['pass'] = ''
 
-apt_reference 'uchiwa' do
+apt_preference 'uchiwa' do
   pin "version #{node['uchiwa']['version']}"
   pin_priority '700'
 end
