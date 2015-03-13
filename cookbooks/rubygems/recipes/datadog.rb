d75ceebf013832c14e9803814808c2dad48a76bf
David Radcliffe <radcliffe.david@gmail.com>
2015-03-12 21:54:18 -0400

    move rubygems-metrics::default into rubygems::datadog

--- cookbooks/rubygems/recipes/datadog.rb
+++ cookbooks/rubygems/recipes/datadog.rb
@@ -1,25 +1,9 @@
 #
-# Cookbook Name:: rubygems-metrics
-# Recipe:: default
+# Cookbook Name:: rubygems
+# Recipe:: datadog
 #
 
 include_recipe 'chef-vault'
-
-package 'collectd' do
-  action :remove
-end
-
-%w(
-  /etc/collectd
-  /usr/lib/collectd
-  /opt/src/collectd-librato-0.0.10
-  /var/lib/collectd
-).each do |dir|
-  directory dir do
-    action :delete
-    recursive true
-  end
-end
 
 datadog_creds = chef_vault_item('datadog', 'credentials')
 
