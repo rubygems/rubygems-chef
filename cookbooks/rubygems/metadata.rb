5d8ffa6c9e7406d4473791e2bfc42772cf5cacd5
Sam Kottler <shk@linux.com>
2015-03-14 15:12:44 -0700

    remove collectd hack since it's been applied everywhere

--- cookbooks/rubygems/metadata.rb
+++ cookbooks/rubygems/metadata.rb
@@ -2,7 +2,7 @@ name             'rubygems'
 maintainer       'RubyGems.org Ops Team'
 license          'MIT'
 description      'RubyGems base recipes'
-version          '0.0.39'
+version          '0.0.40'
 
 depends 'chef-vault'
 depends 'datadog'
