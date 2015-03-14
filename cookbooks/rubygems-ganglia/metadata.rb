96e727583d1a3dc6234818f937d7f9636005911c
Sam Kottler <shk@linux.com>
2015-03-14 15:44:55 -0700

    remove ganglia

--- cookbooks/rubygems-ganglia/metadata.rb
+++
@@ -1,6 +0,0 @@
-name 'rubygems-ganglia'
-
-version '0.0.1'
-
-depends 'chef-vault'
-depends 'ganglia'
--- cookbooks/rubygems-ganglia/recipes/client.rb
+++
@@ -1,4 +0,0 @@
-node.set['ganglia']['version'] = '3.3.7'
-node.set['ganglia']['spoof_hostname'] = true
-
-include_recipe 'ganglia'
