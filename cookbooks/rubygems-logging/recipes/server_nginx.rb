98d145f24bcdbd6f4e7a52af9d102a9db4698bde
David Radcliffe <radcliffe.david@gmail.com>
2015-05-12 00:03:17 -0400

    add nginx password for logs

--- cookbooks/rubygems-logging/recipes/server_nginx.rb
+++ cookbooks/rubygems-logging/recipes/server_nginx.rb
@@ -32,6 +32,15 @@ file "#{node['nginx']['dir']}/certs/rubygems.org.crt" do
   notifies :reload, 'service[nginx]'
 end
 
+auth_bag = chef_vault_item('apps', 'kibana')
+
+file "#{node['nginx']['dir']}/htpasswd" do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  content "#{auth_bag['user']}:#{auth_bag['htpasswd']}"
+end
+
 template "#{node['nginx']['dir']}/sites-available/kibana" do
   source 'nginx.conf.erb'
   owner  'root'
