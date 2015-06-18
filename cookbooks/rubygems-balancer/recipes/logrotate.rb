92f1509802775c4160d86314b6e3e74135952d7a
Andre Medeiros <me@andremedeiros.info>
2015-06-18 17:33:06 -0400

    Reduce the amount of logs we keep around to 30 days

--- cookbooks/rubygems-balancer/recipes/logrotate.rb
+++ cookbooks/rubygems-balancer/recipes/logrotate.rb
@@ -6,7 +6,7 @@
 logrotate_app 'nginx' do
   path "#{node['nginx']['log_dir']}/*.log"
   frequency 'daily'
-  rotate 52
+  rotate 30
   options %w(missingok compress delaycompress notifempty sharedscripts)
   postrotate "    [ -f #{node['nginx']['pid']} ] && kill -USR1 `cat #{node['nginx']['pid']}`"
   create '0640 www-data adm'
