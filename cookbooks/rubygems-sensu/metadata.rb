4c158f6ddec7b4beca1a34b0a09c28da997f2b54
David Radcliffe <radcliffe.david@gmail.com>
2016-01-02 22:21:18 -0500

    update sensu client, server and cookbook

--- cookbooks/rubygems-sensu/metadata.rb
+++ cookbooks/rubygems-sensu/metadata.rb
@@ -1,14 +1,14 @@
 name 'rubygems-sensu'
 maintainer 'RubyGems.org ops team'
 
-version '0.1.46'
+version '0.1.48'
 
 depends 'build-essential'
 depends 'chef-vault'
 depends 'dnsimple'
 depends 'rubygems'
 depends 'rubygems-auth-proxy'
-depends 'sensu', '2.1.0'
+depends 'sensu', '2.11.0'
 depends 'uchiwa', '1.0.0'
 
 supports 'ubuntu'
