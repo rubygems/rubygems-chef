1ab7e3c6836c05c2677150d886a74ecf6294765b
David Radcliffe <radcliffe.david@gmail.com>
2016-01-04 20:58:23 -0500

    upgrade chef server to 12.3.1-1

--- cookbooks/rubygems-chef-server/metadata.rb
+++ cookbooks/rubygems-chef-server/metadata.rb
@@ -1,6 +1,6 @@
 name 'rubygems-chef-server'
 
-version '0.0.5'
+version '0.0.6'
 
 depends 'chef-server'
 depends 'chef-vault'
--- cookbooks/rubygems-chef-server/recipes/default.rb
+++ cookbooks/rubygems-chef-server/recipes/default.rb
@@ -4,7 +4,7 @@
 #
 
 node.default['chef-server']['api_fqdn'] = 'chef.rubygems.org'
-node.default['chef-server']['version'] = '12.2.0-1'
+node.default['chef-server']['version'] = '12.3.1-1'
 node.default['chef-server']['addons'] = ['reporting']
 
 node.default['chef-server']['configuration'] = <<-EOS
