cc5068791021bb5bf7aad957130b40a20d9a8eff
David Radcliffe <radcliffe.david@gmail.com>
2016-12-13 23:11:10 -0500

    remove unused cookbook dependency

--- cookbooks/rubygems-hostname/metadata.rb
+++ cookbooks/rubygems-hostname/metadata.rb
@@ -2,10 +2,9 @@ name             'rubygems-hostname'
 maintainer       'RubyGems.org Ops Team'
 license          'MIT'
 description      'Set the hostname of RubyGems.org machines using node.name'
-version          '0.1.20'
+version          '0.1.21'
 
 depends 'chef-vault'
 depends 'dns'
-depends 'hostname'
 
 supports 'ubuntu'
