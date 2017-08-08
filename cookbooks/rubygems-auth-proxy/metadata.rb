a73bea48c6d3deaa7163f4fee47616e8585fc685
David Radcliffe <radcliffe.david@gmail.com>
2017-08-07 21:29:17 -0400

    update oauth2_proxy to version 2.2.0

--- cookbooks/rubygems-auth-proxy/metadata.rb
+++ cookbooks/rubygems-auth-proxy/metadata.rb
@@ -1,5 +1,5 @@
 name 'rubygems-auth-proxy'
-version '0.0.9'
+version '0.0.10'
 
 depends 'chef_nginx', '~> 3.0.0'
 depends 'ark'
--- cookbooks/rubygems-auth-proxy/recipes/default.rb
+++ cookbooks/rubygems-auth-proxy/recipes/default.rb
@@ -4,8 +4,8 @@
 #
 
 ark 'oauth2_proxy' do
-  url 'https://github.com/bitly/oauth2_proxy/releases/download/v2.0.1/oauth2_proxy-2.0.1.linux-amd64.go1.4.2.tar.gz'
-  version '2.0.1'
-  checksum 'c6d8f6d74e1958ce1688f3cf7d60648b9d0d6d4344d74c740c515a00b4e023ad'
+  url 'https://github.com/bitly/oauth2_proxy/releases/download/v2.2/oauth2_proxy-2.2.0.linux-amd64.go1.8.1.tar.gz'
+  version '2.2.0'
+  checksum '1c16698ed0c85aa47aeb80e608f723835d9d1a8b98bd9ae36a514826b3acce56'
   has_binaries ['oauth2_proxy']
 end
